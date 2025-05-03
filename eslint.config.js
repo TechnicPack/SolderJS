0b207473752ae26668962cd1d99fd9629a53cbc0
Pedro Cunha <pedroagracio@gmail.com>
2025-05-03 21:32:25 +0100

    chore(deps): upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const globals = require('globals');
+const js = require('@eslint/js');
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      indent: ['error', 2],
+      'no-shadow': ['error', { builtinGlobals: true, allow: ['err'] }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+    },
+  },
+  eslintConfigPrettier,
+];
