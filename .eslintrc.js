0ca23d38bf4d2c16da5739e676ad23d48b95ecc5
Pedro Cunha <pedroagracio@gmail.com>
2025-05-03 18:12:10 +0100

    ci(eslint): set ecmaVersion to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
         "node": true
     },
     "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
     "rules": {
         "indent": [
             "error",
