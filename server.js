bb5294b2a48f40d50295917bea2b7768c829a802
Pedro Cunha <pedroagracio@gmail.com>
2025-05-03 19:00:42 +0100

    refactor: introduce arrow functions

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.enable('trust proxy');
 app.disable('x-powered-by');
 
 // Set key and client if available
-app.use(function (req, response, next) {
+app.use((req, response, next) => {
   const key = req.query.k;
   const cid = req.query.cid;
 
@@ -61,8 +61,8 @@ app.use(function (req, response, next) {
 
   async.parallel(
     [
-      function (callback) {
-        getKeys(function (err, keys) {
+      (callback) => {
+        getKeys((err, keys) => {
           if (err) {
             return callback(err, null);
           }
@@ -76,8 +76,8 @@ app.use(function (req, response, next) {
           return callback();
         });
       },
-      function (callback) {
-        getClients(function (err, clients) {
+      (callback) => {
+        getClients((err, clients) => {
           if (err) {
             return callback(err, null);
           }
@@ -85,7 +85,7 @@ app.use(function (req, response, next) {
           if (clients.includes(cid)) {
             req.access.client = { authed: true, client: cid };
             log('info', 'Auth', 'Authenticated Client ID: ' + cid);
-            getClientAccess(cid, function (err, modpacks) {
+            getClientAccess(cid, (err, modpacks) => {
               if (err) {
                 return callback(err, null);
               }
@@ -100,7 +100,7 @@ app.use(function (req, response, next) {
         });
       },
     ],
-    function (err) {
+    (err) => {
       if (err) {
         log('error', 'Auth', 'Error during authentication processing.', err);
       }
@@ -109,18 +109,18 @@ app.use(function (req, response, next) {
   );
 });
 
-app.get('/api', function (req, response) {
-  return response.status(200).json({ api: 'SolderJS', version: '1.0.0', stream: 'stable' }).end();
+app.get('/api', (req, response) => {
+  response.status(200).json({ api: 'SolderJS', version: '1.0.0', stream: 'stable' });
 });
 
-app.get('/api/modpack', function (req, response) {
+app.get('/api/modpack', (req, response) => {
   const options = {
     include: req.query.include,
   };
 
   const apiResponse = {};
 
-  getModpacks(function (err, modpacks) {
+  getModpacks((err, modpacks) => {
     if (err) {
       return response.status(500).json({ error: 'An error has occurred' });
     }
@@ -128,7 +128,7 @@ app.get('/api/modpack', function (req, response) {
     apiResponse.modpacks = {};
     apiResponse.mirror_url = config.url.mirror;
 
-    modpacks.forEach(function (modpack) {
+    modpacks.forEach((modpack) => {
       if (modpack.hidden) {
         if (req.access.key.authed) {
           apiResponse.modpacks[modpack.slug] = modpack.name;
@@ -146,9 +146,9 @@ app.get('/api/modpack', function (req, response) {
       // Grab modpack builds async
       async.each(
         modpacks,
-        function (modpack, callback) {
+        (modpack, callback) => {
           if (apiResponse.modpacks[modpack.slug]) {
-            getModpackResponse(modpack, req, function (err, mObject) {
+            getModpackResponse(modpack, req, (err, mObject) => {
               if (err) {
                 return callback(err);
               }
@@ -159,7 +159,7 @@ app.get('/api/modpack', function (req, response) {
             callback();
           }
         },
-        function (err) {
+        (err) => {
           if (err) {
             return log('error', 'Database', 'Error retrieving builds for modpacks');
           }
@@ -173,25 +173,23 @@ app.get('/api/modpack', function (req, response) {
   });
 });
 
-app.get('/api/modpack/:modpack', function (req, response) {
+app.get('/api/modpack/:modpack', (req, response) => {
   const slug = req.params.modpack;
 
-  getModpack(slug, function (err, modpack) {
+  getModpack(slug, (err, modpack) => {
     if (err) {
       return response.status(500).json({ error: 'An error has occured' });
     }
 
     if (modpack) {
-      getModpackResponse(modpack, req, function (err, res) {
-        return response.status(200).json(res);
-      });
+      getModpackResponse(modpack, req, (err, res) => response.status(200).json(res));
     } else {
       return response.status(404).json({ status: 404, error: 'Modpack does not exist' });
     }
   });
 });
 
-app.get('/api/modpack/:modpack/:build', function (req, response) {
+app.get('/api/modpack/:modpack/:build', (req, response) => {
   const slug = req.params.modpack;
   const build = req.params.build;
 
@@ -199,13 +197,13 @@ app.get('/api/modpack/:modpack/:build', function (req, response) {
     include: req.query.include,
   };
 
-  getModpack(slug, function (err, modpack) {
+  getModpack(slug, (err, modpack) => {
     if (err) {
       return response.status(500).json({ error: 'An error has occured' });
     }
 
     if (modpack) {
-      getBuild(modpack, build, function (err, build) {
+      getBuild(modpack, build, (err, build) => {
         if (err) {
           return response.status(500).json({ error: 'An error has occured' });
         }
@@ -215,9 +213,7 @@ app.get('/api/modpack/:modpack/:build', function (req, response) {
             build.is_published &&
             (!build.private || req.access.key.authed || req.access.client.modpacks.includes(modpack.id))
           ) {
-            getBuildResponse(modpack, build, options, function (err, bObject) {
-              return response.status(200).json(bObject);
-            });
+            getBuildResponse(modpack, build, options, (err, bObject) => response.status(200).json(bObject));
           } else {
             return response.status(403).json({ status: 403, error: 'You are not authorized to view this build.' });
           }
@@ -231,10 +227,10 @@ app.get('/api/modpack/:modpack/:build', function (req, response) {
   });
 });
 
-app.get('/api/verify/:key', function (req, response) {
+app.get('/api/verify/:key', (req, response) => {
   const key = req.params.key;
 
-  getKey(key, function (err, key) {
+  getKey(key, (err, key) => {
     if (err) {
       return response.status(500).json({ error: 'An error has occured' });
     }
@@ -256,13 +252,13 @@ function getModpackResponse(modpack, req, callback) {
     builds: [],
   };
 
-  getBuilds(modpack, function (err, builds) {
+  getBuilds(modpack, (err, builds) => {
     if (err) {
       callback(err, null);
       return log('error', 'Modpack', 'Failed to get builds while building modpack response', err);
     }
 
-    builds.forEach(function (build) {
+    builds.forEach((build) => {
       if (
         build.is_published &&
         (!build.private || req.access.key.authed || req.access.client.modpacks.includes(modpack.id))
@@ -284,13 +280,13 @@ function getBuildResponse(modpack, build, options, callback) {
     mods: [],
   };
 
-  getMods(build, function (err, mods) {
+  getMods(build, (err, mods) => {
     if (err) {
       callback(err, null);
       return log('error', 'Mods', 'Failed to get mods while building build response', err);
     }
 
-    mods.forEach(function (mod) {
+    mods.forEach((mod) => {
       const modObject = {
         name: mod.name,
         version: mod.version,
@@ -317,12 +313,12 @@ function getBuildResponse(modpack, build, options, callback) {
 }
 
 function getKeys(callback) {
-  rclient.get('api:access:keys', function (err, res) {
+  rclient.get('api:access:keys', (err, res) => {
     if (res) {
       callback(null, JSON.parse(res));
       log('debug', 'Cache', 'Loaded keys');
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -330,7 +326,7 @@ function getKeys(callback) {
 
         const query = 'SELECT * FROM keys';
 
-        client.query(query, function (err, result) {
+        client.query(query, (err, result) => {
           done();
 
           if (err) {
@@ -351,12 +347,12 @@ function getKeys(callback) {
 }
 
 function getClients(callback) {
-  rclient.get('api:access:clients', function (err, res) {
+  rclient.get('api:access:clients', (err, res) => {
     if (res) {
       callback(null, JSON.parse(res));
       log('debug', 'Cache', 'Loaded clients');
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -364,7 +360,7 @@ function getClients(callback) {
 
         const query = 'SELECT * FROM clients';
 
-        client.query(query, function (err, result) {
+        client.query(query, (err, result) => {
           done();
 
           if (err) {
@@ -385,7 +381,7 @@ function getClients(callback) {
 }
 
 function getClientAccess(cid, callback) {
-  pool.connect(function (err, client, done) {
+  pool.connect((err, client, done) => {
     if (err) {
       callback(err, null);
       return log('error', 'Database', 'Error fetching client from pool', err);
@@ -395,7 +391,7 @@ function getClientAccess(cid, callback) {
       'SELECT * FROM clients JOIN client_modpack ON clients.id = client_modpack.client_id WHERE clients.uuid=$1';
     const data = [cid];
 
-    client.query(query, data, function (err, result) {
+    client.query(query, data, (err, result) => {
       done();
 
       if (err) {
@@ -411,12 +407,12 @@ function getClientAccess(cid, callback) {
 }
 
 function getModpacks(callback) {
-  rclient.get('api:modpacks', function (err, res) {
+  rclient.get('api:modpacks', (err, res) => {
     if (res) {
       log('debug', 'Cache', 'Loaded modpacks');
       callback(null, JSON.parse(res));
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -424,7 +420,7 @@ function getModpacks(callback) {
 
         const query = 'SELECT * FROM modpacks ORDER BY id ASC';
 
-        client.query(query, function (err, result) {
+        client.query(query, (err, result) => {
           done();
 
           if (err) {
@@ -445,12 +441,12 @@ function getModpacks(callback) {
 }
 
 function getModpack(slug, callback) {
-  rclient.get('api:modpack:' + slug, function (err, res) {
+  rclient.get('api:modpack:' + slug, (err, res) => {
     if (res) {
       log('debug', 'Cache', 'Loaded modpack', slug);
       callback(null, JSON.parse(res));
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -459,7 +455,7 @@ function getModpack(slug, callback) {
         const query = 'SELECT * FROM modpacks WHERE slug=$1 ORDER BY id ASC LIMIT 1';
         const data = [slug];
 
-        client.query(query, data, function (err, result) {
+        client.query(query, data, (err, result) => {
           done();
 
           if (err) {
@@ -480,12 +476,12 @@ function getModpack(slug, callback) {
 }
 
 function getBuilds(modpack, callback) {
-  rclient.get('api:modpack:builds:' + modpack.id, function (err, res) {
+  rclient.get('api:modpack:builds:' + modpack.id, (err, res) => {
     if (res) {
       log('debug', 'Cache', 'Loaded builds', modpack.slug);
       callback(null, JSON.parse(res));
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -494,7 +490,7 @@ function getBuilds(modpack, callback) {
         const query = 'SELECT * FROM builds WHERE modpack_id=$1::int ORDER BY id ASC';
         const data = [modpack.id];
 
-        client.query(query, data, function (err, result) {
+        client.query(query, data, (err, result) => {
           done();
 
           if (err) {
@@ -515,12 +511,12 @@ function getBuilds(modpack, callback) {
 }
 
 function getBuild(modpack, build, callback) {
-  rclient.get('api:build:' + modpack.id + ':' + build, function (err, res) {
+  rclient.get('api:build:' + modpack.id + ':' + build, (err, res) => {
     if (res) {
       log('debug', 'Cache', 'Loaded build', [modpack.slug, build]);
       callback(null, JSON.parse(res));
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -529,7 +525,7 @@ function getBuild(modpack, build, callback) {
         const query = 'SELECT * FROM builds WHERE modpack_id=$1::int AND version=$2 LIMIT 1';
         const data = [modpack.id, build];
 
-        client.query(query, data, function (err, result) {
+        client.query(query, data, (err, result) => {
           done();
 
           if (err) {
@@ -550,12 +546,12 @@ function getBuild(modpack, build, callback) {
 }
 
 function getMods(build, callback) {
-  rclient.get('api:mods:' + build.id, function (err, res) {
+  rclient.get('api:mods:' + build.id, (err, res) => {
     if (res) {
       log('debug', 'Cache', 'Loaded mods', build.id);
       callback(null, JSON.parse(res));
     } else {
-      pool.connect(function (err, client, done) {
+      pool.connect((err, client, done) => {
         if (err) {
           callback(err, null);
           return log('error', 'Database', 'Error fetching client from pool', err);
@@ -565,7 +561,7 @@ function getMods(build, callback) {
           'SELECT mods.id, * FROM build_modversion AS bmv INNER JOIN modversions AS mv ON mv.id = bmv.modversion_id INNER JOIN mods ON mods.id = mv.mod_id WHERE bmv.build_id=$1::int ORDER BY mods.name ASC';
         const data = [build.id];
 
-        client.query(query, data, function (err, result) {
+        client.query(query, data, (err, result) => {
           done();
 
           if (err) {
@@ -586,7 +582,7 @@ function getMods(build, callback) {
 }
 
 function getKey(key, callback) {
-  pool.connect(function (err, client, done) {
+  pool.connect((err, client, done) => {
     if (err) {
       callback(err, null);
       return log('error', 'Database', 'Error fetching client from pool', err);
@@ -595,7 +591,7 @@ function getKey(key, callback) {
     const query = 'SELECT * FROM keys WHERE api_key=$1 LIMIT 1';
     const data = [key];
 
-    client.query(query, data, function (err, result) {
+    client.query(query, data, (err, result) => {
       done();
 
       if (err) {
